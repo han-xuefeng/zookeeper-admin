810b9078d0e2f32c01ad0d03e98d91258fef78a7
han-xuefeng <han-xuefeng@hotmail.com>
2023-04-04 17:01:57 +0800

    init code

---
+++ src/main/java/com/idaiq/zookeeperAdmin/ZookeeperAdminApplication.java
@@ -0,0 +1,13 @@
+package com.idaiq.zookeeperAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idaiq/zookeeperAdmin/ZookeeperAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idaiq.zookeeperAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
